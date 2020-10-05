2d38ce7650644e4ce9cf5e31a534255f4ca418a2
Liam Nelski <ljnelski@gmail.com>
2020-10-05 17:21:32 -0400

    Added rudimentary player movement and camera control
    If you want to check it out, open the test level scene
    - Added some test sprites
    - Added Player Movement script
    - Added Camera control script

---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Private Varibles
+    [SerializeField] private Camera cam;
+
+    [SerializeField] private float speed;
+    [SerializeField] private Transform flameJet;
+    private Rigidbody2D rbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Mouse 0 value: " + Input.GetAxis("Mouse 0"));
+
+
+        if (Input.GetAxis("Mouse 0") > 0.0f)
+        {
+            rbody.AddForce(GetJetDirection() * speed);
+        }
+        PointJet(GetJetDirection());
+    }
+    private Vector2 GetJetDirection()
+    {
+        Vector2 jetDir = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.DrawRay(transform.position, jetDir.normalized, Color.white);
+        return jetDir.normalized;          
+    }
+    private void PointJet(Vector2 dir)
+    {
+        flameJet.eulerAngles = Vector3.forward * (Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg);
+    }
+   
+}
