27852bcfa676857d408e61fd9a155e8dd7c1f879
Chloe <chloe_baker@live.com>
2020-10-13 21:12:12 -0400

    Created Basic Main Menu
    - created main menu and level select
    - created MainMenu script
    - put in placeholder background
    - added submit, cancel, horizontal, and vertical input for menu navigation
    - created scenes for each level
    - added menu and level scenes to the build index

---
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //to be able to load scenes
+
+public class MainMenu : MonoBehaviour
+{
+    // Play 
+    public void PlayLevel1() 
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //load next scene in queue
+        SceneManager.LoadScene("Glowing Cave"); //will load specific scene by name
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("Rainy Forest");
+    }
+
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene("Quaking Bog");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT--");
+        Application.Quit();
+    }
+}
