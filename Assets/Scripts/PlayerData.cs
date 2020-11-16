8f72eaee5a40dfa7ca8eafaf442da4a4a5ef754d
Chloe <chloe_baker@live.com>
2020-11-16 15:38:59 -0500

    Respawn UI
    - added UI as child of player
    - changed PlayerSaveLoad script
    - commented out checkpoint stuff in PlayerData, PlayerSaveLoad

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -9,13 +9,13 @@ public class PlayerData //no monobehaviour
     // This class just gets player data to save the level progress. SaveSystem does the actual saving.
 
     public int level = 1; //player's current level, 1 by default
-    public string checkpoint = "1_BonfireCheckpoint"; //player's current checkpoint name
+    //public string checkpoint = "1_BonfireCheckpoint"; //player's current checkpoint name
     //public float[] spawn; //checkpoint spawn
 
     public PlayerData(PlayerSaveLoad player)
     {
         level = player.currentLevel; //getting the current level from PlayerController
-        checkpoint = player.currentCheckpoint; //current checkpoint from PlayerController
+        //checkpoint = player.currentCheckpoint; //current checkpoint from PlayerController
 
         //spawn = new float[3];
         //spawn[0] = player.transform.position.x;
