834b305a4be820d5cce7077bb644352ca5569a46
Ethan <estloui2@my.centennialcollege.ca>
2020-11-20 15:31:22 -0500

    Placed wind traps and made water drop traps

---
+++ Assets/TrapSpawnerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSpawnerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject trap;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Instantiate (trap, transform.position, Quaternion.identity);
+        }
+        
+    }
+}
