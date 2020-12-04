27ea97c89feb9d598f387486854844bc703db2bb
Chloe <chloe_baker@live.com>
2020-12-04 13:42:09 -0500

    Chest
    - created chest animation (open/closed)
    - ChestTrigger script to open/close the chest
    - player automatically opens chest when they touch it
    - placed one in level 1 and level 2 (I think in the right spot?)

---
+++ Assets/Scripts/ChestTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestTrigger : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) //if you enter the bounding box
+            anim.SetBool("nearChest", true);
+    }
+}
