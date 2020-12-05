a33cef312d0b833a4a0a0a6ea5712c00e5840640
Chloe <chloe_baker@live.com>
2020-12-04 19:13:17 -0500

    End Text
    - end text stays until the player walks past it, then it goes away

---
+++ Assets/Scripts/MenuScripts/TextOff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextOff : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject.Find("EndTextCanvas").SetActive(false); //disable itself
+        }
+    }
+}
